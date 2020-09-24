03830d46f3339845864b7b723993eb9f31fcad9c
qushuai <369406380@qq.com>
2020-09-24 11:58:33 +0800

    第一次

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.GET("/someGet", getting)
+	r.Run() // listen and serve on 0.0.0.0:8080
+}
+
+func getting(c *gin.Context) {
+	var aa = [...]int{1, 2, 3}
+	var bb = []int{1, 2, 3}
+	//aa[3] = 4
+	bb[3] = 4
+	fmt.Println(aa)
+	fmt.Println(bb)
+	fmt.Printf("%v类型：%T\n", aa, aa)
+	fmt.Printf("类型：%T\n", bb)
+	c.JSON(200, gin.H{
+		"message": "someGet",
+	})
+}
